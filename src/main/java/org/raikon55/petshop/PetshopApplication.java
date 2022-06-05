8e712d06f056689a5c92ec5d0a111d7f27a00323
raikon55 <edufelopes@gmail.com>
2022-06-05 10:13:37 -0300

    Testar fachada categorias

--- src/main/java/org/raikon55/petshop/PetshopApplication.java
+++ src/main/java/org/raikon55/petshop/PetshopApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PetshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetshopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PetshopApplication.class, args);
+    }
 
 }
