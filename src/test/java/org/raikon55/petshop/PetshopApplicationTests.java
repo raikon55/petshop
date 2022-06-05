56dfd2395f8247b79fca3d96a57367f8b3269a79
raikon55 <edufelopes@gmail.com>
2022-06-05 09:56:08 -0300

    Projeto Criado

---
+++ src/test/java/org/raikon55/petshop/PetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.raikon55.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
