38c3ce2f19bf6b6d0b0dc7d65c5169793c1f9189
raikon55 <edufelopes@gmail.com>
2022-06-05 14:03:09 -0300

    Adicionado classe de Modelo Categoria e Rest

--- src/main/java/org/raikon55/petshop/resources/CategoriaResource.java
+++ src/main/java/org/raikon55/petshop/resources/CategoriaResource.java
@@ -1,5 +1,9 @@
 package org.raikon55.petshop.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.raikon55.petshop.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-    
+
     @GetMapping
-    public String mostrar() {
-        return "Teste REST categorias";
+    public List<Categoria> mostrar() {
+        Categoria cat1 = new Categoria(1, "Alimento");
+        Categoria cat2 = new Categoria(2, "Cosmético");
+        Categoria cat3 = new Categoria(3, "Remédio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+        lista.add(cat3);
+        return lista;
     }
 }
